ebb515f7b2c89855510fb467e09008c38de0be16
nsonidotdev <anton.stecyura.pr1@gmail.com>
2025-01-19 14:07:21 +0200

    fix: update flag schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
-export const flagSchema = z.any()
+export const flagSchema = z.string().or(z.undefined())
     .refine((v) => {
-        if (v === true) return true;
-        if (v == null) return true;
+        if (v === "" || v === undefined) return true;
         return false;
-    }, { message: "Flag can't take any values" }).transform(v => {
-        if (v === true) return true;
+    }, { message: "Flag option can't take any values" })
+    .transform(v => {
+        if (v === "") return true;
+        if (v === undefined) return false;
         return false;
     });
