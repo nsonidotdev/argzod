14d4b84d087bbc7c492e27788dceff0313925fb7
nsonidotdev <anton.stecyura.pr1@gmail.com>
2025-01-21 13:55:37 +0200

    chore(gh_actions): remove fail test

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,3 @@ import * as argzod from './external';
 export * from './external';
 export default argzod;
 export { argzod }
-
-export const add = (a: number, b: number) => {
-    return a + b;
-}
-
-add(1, 'Hello, world!')
