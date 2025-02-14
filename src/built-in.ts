58a207f9cb61eb898517762d0140099a245b9b74
nsonidotdev <anton.stecyura.pr1@gmail.com>
2025-02-14 11:34:00 +0200

    fix: remove flagSchema usage

--- src/built-in.ts
+++ src/built-in.ts
@@ -1,10 +1,8 @@
 import { OptionParseType } from './enums';
-import { schemas } from './schemas';
 import type { OptionDef } from './types/option-def';
 
 export const helpOption: OptionDef = {
     parse: OptionParseType.Boolean,
-    schema: schemas.flagSchema,
     description: 'Logs out detailed infromation about a command',
     name: ['help', 'h'],
 };
